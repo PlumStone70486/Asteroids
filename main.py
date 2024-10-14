f93cee8cb52545af61f47affe07ab80f2536eecb
PlumStone70486 <tsbg95@gmail.com>
2024-10-14 16:06:59 +0200

    add player and circleshape, trying to create the player

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 import pygame
 from constants import *
+from player import *
 def main():
 	pygame.init()
+	clock = pygame.time.Clock()
+	dt = 0
+	player = Player(2, 2, PLAYER_RADIUS)
 	print("Starting asteroids!")
 	print(f"Screen width: {SCREEN_WIDTH}")
 	print(f"Screen height: {SCREEN_HEIGHT}")
@@ -10,6 +14,9 @@ def main():
 		black = (0, 0, 0)
 		screen.fill(black)
 		pygame.display.flip()
+		clock.tick(60)
+		dt = clock.get_time() / 1000
+		player.draw(screen)
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
 				return
